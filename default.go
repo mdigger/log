43ed38213b2ce7abd2ccf8cd2b663e5042ef1f70
Дмитрий Седых <dmitrys@xyzrd.com>
2016-09-21 10:15:01 +0300

    Default logger

--- default.go
+++ default.go
@@ -9,66 +9,66 @@ import (
 
 var (
 	plainHandler = NewPlainHandler(os.Stdout, LstdFlags)
-	std          = New(plainHandler)
+	Default      = New(plainHandler)
 )
 
 // WithFields creates a new context for logging, adding a new field list.
 func WithFields(fields Fields) *Context {
-	return std.WithFields(fields)
+	return Default.WithFields(fields)
 }
 
 // WithField creates a new context for logging, adding the new named field.
 func WithField(name string, value interface{}) *Context {
-	return std.WithField(name, value)
+	return Default.WithField(name, value)
 }
 
 // WithError creates a new context for logging, adding the error field.
 func WithError(err error) *Context {
-	return std.WithError(err)
+	return Default.WithError(err)
 }
 
 // WithSource creates a new context for logging, adding the caller source field.
 func WithSource(calldepth int) *Context {
-	return std.WithSource(calldepth)
+	return Default.WithSource(calldepth)
 }
 
 // Debug displays the debug message in the log.
 func Debug(message string) {
-	std.print(DebugLevel, message)
+	Default.print(DebugLevel, message)
 }
 
 // Debugf displays the debug formatted message in the log.
 func Debugf(format string, v ...interface{}) {
-	std.print(DebugLevel, fmt.Sprintf(format, v...))
+	Default.print(DebugLevel, fmt.Sprintf(format, v...))
 }
 
 // Info displays the message in the log.
 func Info(message string) {
-	std.print(InfoLevel, message)
+	Default.print(InfoLevel, message)
 }
 
 // Infof displays the formatted message in the log.
 func Infof(format string, v ...interface{}) {
-	std.print(InfoLevel, fmt.Sprintf(format, v...))
+	Default.print(InfoLevel, fmt.Sprintf(format, v...))
 }
 
 // Error displays the error message in the log.
 func Error(message string) {
-	std.print(ErrorLevel, message)
+	Default.print(ErrorLevel, message)
 }
 
 // Errorf displays the formatted error message in the log.
 func Errorf(format string, v ...interface{}) {
-	std.print(ErrorLevel, fmt.Sprintf(format, v...))
+	Default.print(ErrorLevel, fmt.Sprintf(format, v...))
 }
 
 // Trace returns a new entry with a Stop method to fire off a corresponding
 // completion log, useful with defer.
 func Trace(message string) *TraceContext {
-	std.print(InfoLevel, message)
+	Default.print(InfoLevel, message)
 	return &TraceContext{
 		Message: message,
-		context: std.Context,
+		context: Default.Context,
 		started: time.Now(),
 	}
 }
@@ -77,10 +77,10 @@ func Trace(message string) *TraceContext {
 // and returns the trace context to further it is stopped by Stop method.
 func Tracef(format string, v ...interface{}) *TraceContext {
 	message := fmt.Sprintf(format, v...)
-	std.print(InfoLevel, message)
+	Default.print(InfoLevel, message)
 	return &TraceContext{
 		Message: message,
-		context: std.Context,
+		context: Default.Context,
 		started: time.Now(),
 	}
 }
