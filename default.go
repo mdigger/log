930469b38c7689ae3b07745aa047af446137c667
Дмитрий Седых <dmitrys@xyzrd.com>
2016-09-24 19:29:04 +0300

    Default log context

--- default.go
+++ default.go
@@ -33,18 +33,19 @@ func SetOutput(w io.Writer) {
 	consoleHandler.SetOutput(w)
 }
 
-var defaulContext = consoleHandler.Context()
+// Default is default console log Context.
+var Default = consoleHandler.Context()
 
 func WithFields(fields Fields) *Context {
-	return defaulContext.WithFields(fields)
+	return Default.WithFields(fields)
 }
 
 func WithField(name string, value interface{}) *Context {
-	return defaulContext.WithField(name, value)
+	return Default.WithField(name, value)
 }
 
 func WithError(err error) *Context {
-	return defaulContext.WithError(err)
+	return Default.WithError(err)
 }
 
 // WithSource return new Context with added information about the file name and
@@ -52,55 +53,55 @@ func WithError(err error) *Context {
 // frames to skip when computing the file name and line number. A value of 0
 // will print the details for the caller.
 func WithSource(calldepth int) *Context {
-	return defaulContext.WithSource(calldepth + 1)
+	return Default.WithSource(calldepth + 1)
 }
 
 // Info publishes the informational message to the default log.
 func Info(message string) error {
-	return defaulContext.print(InfoLevel, message)
+	return Default.print(InfoLevel, message)
 }
 
 // Infof publishes the formatted informational message to the default log.
 func Infof(format string, v ...interface{}) error {
-	return defaulContext.print(InfoLevel, fmt.Sprintf(format, v...))
+	return Default.print(InfoLevel, fmt.Sprintf(format, v...))
 }
 
 // Debug publishes the debug message to the default log.
 func Debug(message string) error {
-	return defaulContext.print(DebugLevel, message)
+	return Default.print(DebugLevel, message)
 }
 
 // Debugf publishes the formatted debug message to the default log.
 func Debugf(format string, v ...interface{}) error {
-	return defaulContext.print(DebugLevel, fmt.Sprintf(format, v...))
+	return Default.print(DebugLevel, fmt.Sprintf(format, v...))
 }
 
 // Warning publishes the warning message to the default log.
 func Warning(message string) error {
-	return defaulContext.print(WarningLevel, message)
+	return Default.print(WarningLevel, message)
 }
 
 // Warningf publishes the formatted warning message to the default log.
 func Warningf(format string, v ...interface{}) error {
-	return defaulContext.print(WarningLevel, fmt.Sprintf(format, v...))
+	return Default.print(WarningLevel, fmt.Sprintf(format, v...))
 }
 
 // Error publishes the error message to the default log.
 func Error(message string) error {
-	return defaulContext.print(ErrorLevel, message)
+	return Default.print(ErrorLevel, message)
 }
 
 // Errorf publishes the formatted error message to the default log.
 func Errorf(format string, v ...interface{}) error {
-	return defaulContext.print(ErrorLevel, fmt.Sprintf(format, v...))
+	return Default.print(ErrorLevel, fmt.Sprintf(format, v...))
 }
 
 // Trace sends to the default log information message and returns a new
 // TraceContext with a Stop method to fire off a corresponding completion log.
 // Useful with defer.
 func Trace(message string) *TraceContext {
-	defaulContext.print(InfoLevel, message)
-	return defaulContext.trace(message)
+	Default.print(InfoLevel, message)
+	return Default.trace(message)
 }
 
 // Trace sends to the default log formatted information message and returns a
@@ -108,6 +109,6 @@ func Trace(message string) *TraceContext {
 // log. Useful with defer.
 func Tracef(format string, v ...interface{}) *TraceContext {
 	message := fmt.Sprintf(format, v...)
-	defaulContext.print(InfoLevel, message)
-	return defaulContext.trace(message)
+	Default.print(InfoLevel, message)
+	return Default.trace(message)
 }
